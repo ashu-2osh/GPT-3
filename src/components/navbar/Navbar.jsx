90482bff7fe2971cbaffaa12dc7de823209f07bb
Ashutosh Sahoo <ashutosh.sahoo@apmosys.com>
2026-02-04 16:07:17 +0530

    update navbat text

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { RiMenu3Line, RiCloseLine } from "react-icons/ri"
 const Menu = () => (
   <>
     <p><a href='#home'>Home</a></p>
-    <p><a href='#wgpt3'>What is GPT3000000?</a></p>
+    <p><a href='#wgpt3'>What is GPT3000001?</a></p>
     <p><a href='#possibility'>Open AI</a></p>
     <p><a href='#features'>Case Studies</a></p>
     <p><a href='#blog'>Library</a></p>
